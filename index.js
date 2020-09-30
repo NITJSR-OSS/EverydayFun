be967278fbf2dbeb33b035cd731e0b1fdfd032cd
tachodril <rajlevel97@gmail.com>
2020-10-01 03:38:30 +0530

    package published

---
+++ index.js
@@ -0,0 +1,34 @@
+var data_quote = require("./data/quotes.json");
+var data_joke = require("./data/jokes.json");
+var data_riddle = require("./data/riddles.json");
+
+var cur = {};
+
+function randomNoGenerator(min, max) {
+  if (typeof max !== "number" && typeof min !== "number") {
+    min = 0;
+    max = 1;
+  }
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+module.exports.getRandomQuote = function () {
+  var ind = randomNoGenerator(0, 490);
+  cur.quote = data_quote[ind].Quote;
+  cur.author = data_quote[ind].Author;
+  return cur;
+};
+
+module.exports.getRandomJoke = function () {
+  var ind = randomNoGenerator(0, 490);
+  cur.body = data_joke[ind].body;
+  cur.category = data_joke[ind].category;
+  return cur;
+};
+
+module.exports.getRandomRiddle = function () {
+  var ind = randomNoGenerator(0, 8);
+  cur.riddle = data_riddle[ind].riddle;
+  cur.answer = data_riddle[ind].answer;
+  return cur;
+};
---
+++ test.js
@@ -0,0 +1,5 @@
+const all = require("./index.js");
+
+//console.log(all.getRandomQuote());
+//console.log(all.getRandomJoke());
+console.log(all.getRandomRiddle());
