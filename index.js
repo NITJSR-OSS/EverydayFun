11d4c41b5f33d8d68f1b8d34c2306390ab6d4f5f
NinjaLabs-Dev <fraser@ninjalabs.co.uk>
2020-10-01 12:15:37 +0100

    Added functionality to search by one/many category/author

--- index.js
+++ index.js
@@ -6,12 +6,54 @@ function getRandomItem(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
-function getRandomQuote() {
-  return getRandomItem(quotes);
+function getRandomQuote(author) {
+  if(author) {
+    list = [];
+    if(typeof author === 'string') {
+      quotes.forEach(function(item) {
+        if(item.Author === author) {
+          list.push(item);
+        }
+      })
+    } else if (typeof author === 'object') {
+      author.forEach(function(auth) {
+        quotes.forEach(function(item) {
+          if(item.Author === auth) {
+            list.push(item);
+          }
+        })
+      })
+    }
+
+    return list;
+  } else {
+    return getRandomItem(quotes);
+  }
 };
 
-function getRandomJoke() {
-  return getRandomItem(jokes);
+function getRandomJoke(category) {
+  if(category) {
+    list = [];
+    if(typeof category === 'string') {
+      jokes.forEach(function(item) {
+        if(item.category === category) {
+          list.push(item);
+        }
+      })
+    } else if (typeof category === 'object') {
+      category.forEach(function(cat) {
+        jokes.forEach(function(item) {
+          if(item.category === cat) {
+            list.push(item);
+          }
+        })
+      })
+    }
+
+    return list;
+  } else {
+    return getRandomItem(jokes);
+  }
 }
 
 function getRandomRiddle() {
